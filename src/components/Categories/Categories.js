637883ef4d5b32c543ebe81a58d1b383b7668fa0
kishibe-rohan <devrohan2000@gmail.com>
2021-07-04 11:12:49 +0530

    designed categories section

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./_categories.scss";
 
+const keywords = [
+  "All",
+  "Among Us",
+  "Javascript",
+  "Music",
+  "React JS",
+  "Computer Science",
+  "Jojo's Bizarre Adventure",
+  "Anime",
+  "Podcasts",
+  "Cricket",
+  "Data Science",
+  "Bodybuilding",
+  "Comedy",
+  "Woodworking",
+  "Tools",
+];
+
 const Categories = () => {
-  return <div>Categories</div>;
+  const [category, setCategory] = useState("All");
+
+  const handleClick = (value) => {
+    setCategory(value);
+  };
+  return (
+    <div className="categories_bar">
+      {keywords.map((value, index) => (
+        <span
+          className={category === value ? "active" : ""}
+          onClick={() => handleClick(value)}
+          key={index}
+        >
+          {value}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
